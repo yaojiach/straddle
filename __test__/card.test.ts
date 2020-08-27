ce19fbd3ba6b3f3deb4a125615a20c3b4d501454
yaojiach <jiachen.yao@outlook.com>
2020-08-27 01:45:50 -0400

    Test create new card

--- __test__/card.test.ts
+++ __test__/card.test.ts
@@ -1,7 +1,7 @@
 import { Card } from '../src/card'
 
 describe('test card class', () => {
-  it('works', () => {
-    console.log(Card.STR_RANK_TO_INT_RANK)
+  it('creates right card', () => {
+    expect(Card.new('Qh')).toEqual(67119647)
   })
 })
--- src/card.ts
+++ src/card.ts
@@ -5,4 +5,29 @@ export class Card {
   static STR_RANK_TO_INT_RANK = Object.fromEntries(
     Card.STR_RANKS.map((_, i) => [Card.STR_RANKS[i], Card.INT_RANKS[i]])
   )
+  static STR_SUIT_TO_INT_SUIT: { [k: string]: number } = {
+    s: 1, // spades
+    h: 2, // hearts
+    d: 4, // diamonds
+    c: 8 // clubs
+  }
+  static INT_SUIT_TO_STR_SUIT = 'xshxdxxxc'.split('')
+  static INT_SUIT_TO_PRETTY_SUIT: { [k: string]: string } = {
+    1: '♠️',
+    2: '♥️',
+    4: '♦️',
+    8: '♣️'
+  }
+
+  static new(id: string): number {
+    const rankInt = Card.STR_RANK_TO_INT_RANK[id[0]]
+    const suitInt = Card.STR_SUIT_TO_INT_SUIT[id[1]]
+    const rankPrime = Card.PRIMES[rankInt]
+
+    const bitRank = (1 << rankInt) << 16
+    const suit = suitInt << 12
+    const rank = rankInt << 8
+
+    return bitRank | suit | rank | rankPrime
+  }
 }
