ccaf4e1ba279dcffd4013a32c9d120e0d19f3f0b
yaojiach <jiachen.yao@outlook.com>
2020-08-27 15:50:52 -0400

    Complete cards

--- __test__/card.test.ts
+++ __test__/card.test.ts
@@ -4,4 +4,20 @@ describe('test card class', () => {
   it('creates right card', () => {
     expect(Card.new('Qh')).toEqual(67119647)
   })
+
+  it('returns the right string from int', () => {
+    expect(Card.intToStr(67119647)).toEqual('Qh')
+  })
+
+  it('returns the right prime product from a hand', () => {
+    expect(Card.primeProductFromHand([Card.new('As'), Card.new('Qh')])).toEqual(1271)
+  })
+
+  it('returns the right prime product from a rank bits', () => {
+    expect(Card.primeProductFromRankBits(7936)).toEqual(31367009)
+  })
+
+  it('returns the right pretty print from int', () => {
+    expect(Card.intToPrettyStr(67119647)).toEqual('[Q♥️]')
+  })
 })
--- src/card.ts
+++ src/card.ts
@@ -30,4 +30,52 @@ export class Card {
 
     return bitRank | suit | rank | rankPrime
   }
+
+  static handToBinary(cardStrs: string[]) {
+    return cardStrs.map(c => Card.new(c))
+  }
+
+  static intToStr(cardInt: number) {
+    return (
+      Card.STR_RANKS[Card.getRankInt(cardInt)] + Card.INT_SUIT_TO_STR_SUIT[Card.getSuitInt(cardInt)]
+    )
+  }
+
+  static getRankInt(cardInt: number) {
+    return (cardInt >> 8) & 0xf
+  }
+
+  static getSuitInt(cardInt: number) {
+    return (cardInt >> 12) & 0xf
+  }
+
+  static getBitRandInt(cardInt: number) {
+    return (cardInt >> 16) & 0x1fff
+  }
+
+  static getPrime(cardInt: number) {
+    return cardInt & 0x3f
+  }
+
+  static primeProductFromHand(cardInts: number[]) {
+    return cardInts.reduce((x, y) => x * (y & 0xff), 1)
+  }
+
+  static primeProductFromRankBits(rankBits: number) {
+    return Card.INT_RANKS.filter(i => rankBits & (1 << i)).reduce((x, y) => x * Card.PRIMES[y], 1)
+  }
+
+  static intToPrettyStr(cardInt: number) {
+    return `[${Card.STR_RANKS[Card.getRankInt(cardInt)]}${
+      Card.INT_SUIT_TO_PRETTY_SUIT[Card.getSuitInt(cardInt)]
+    }]`
+  }
+
+  static printPrettyCard(cardInt: number) {
+    console.log(Card.intToPrettyStr(cardInt))
+  }
+
+  static printPrettyCards(cardInts: number[]) {
+    console.log(cardInts.map(c => Card.intToPrettyStr(c)).join(' '))
+  }
 }
