74e23a448972525c4d3806b88901ebbc4b62ded1
yaojiach <jiachen.yao@outlook.com>
2020-08-27 01:21:07 -0400

    Start working on Card class

---
+++ __test__/card.test.ts
@@ -0,0 +1,7 @@
+import { Card } from '../src/card'
+
+describe('test card class', () => {
+  it('works', () => {
+    console.log(Card.STR_RANK_TO_INT_RANK)
+  })
+})
--- src/card.ts
+++ src/card.ts
@@ -1,1 +1,8 @@
-export class Card {}
+export class Card {
+  static STR_RANKS = '23456789TJQKA'.split('')
+  static INT_RANKS = [...Array(13).keys()]
+  static PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+  static STR_RANK_TO_INT_RANK = Object.fromEntries(
+    Card.STR_RANKS.map((_, i) => [Card.STR_RANKS[i], Card.INT_RANKS[i]])
+  )
+}
