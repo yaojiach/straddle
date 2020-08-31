652055cb792d50fac7938a6a06cd2ffe81641f1f
yaojiach <jiachen.yao@outlook.com>
2020-08-31 14:58:15 -0400

    Provide more info in evaluate return value

--- src/evaluator.ts
+++ src/evaluator.ts
@@ -1,4 +1,4 @@
-import { Lookup } from './lookup'
+import { Lookup, getHandFromScore } from './lookup'
 import { Card } from './card'
 import { combinations } from './helpers'
 
@@ -8,11 +8,11 @@ export class Evaluator {
   evaluate(cards: number[], board: number[]) {
     return combinations(cards.concat(board), 5)
       .map(c => this.five(c))
-      .reduce((x, y) => x.score < y.score ? x : y)
+      .reduce((x, y) => (x.score < y.score ? x : y))
   }
 
   private five(cards: number[]) {
-    let score
+    let score: number
     if (cards[0] & cards[1] & cards[2] & cards[3] & cards[4] & 0xf000) {
       const handOR = (cards[0] | cards[1] | cards[2] | cards[3] | cards[4]) >> 16
       score = LOOKUP.flushLookup[Card.primeProductFromRankBits(handOR)]
@@ -20,8 +20,11 @@ export class Evaluator {
       score = LOOKUP.unSuitedLookup[Card.primeProductFromHand(cards)]
     }
     return {
+      score,
       hand: cards,
-      score
+      handStr: cards.map(c => Card.intToStr(c)),
+      handPrettyStr: cards.map(c => Card.intToPrettyStr(c)),
+      handName: getHandFromScore(score)
     }
   }
 }
